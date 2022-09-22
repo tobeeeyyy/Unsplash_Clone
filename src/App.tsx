2a0c2a915249b18cfabf5c9479522e0cdcf971d0
Dev Pratap Singh <devsingh18003@gmail.com>
2022-09-22 12:45:09 +0530

    Build documented stucture of the website for better readability

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,17 @@
-import React, {useEffect, useState} from 'react'
 import Heading from "./components/Heading"
-// import Loader from "./components/Loader"
-import UnsplashImage from "./components/UnsplashImage"
+// import Loader from "./components/CSSLoader"
 
-import './App.css'
-import axios from "axios"
-import InfiniteScroll from 'react-infinite-scroll-component'
+import './global.css'
+import HomePage from './Page/HomePage'
 
 
 function App() {
 
-  const [images, setImages] = useState<Array<any>>([])
-
-  useEffect( ()=> {
-    fetchImages();
-  }, [])
-
-  const fetchImages = () => {
-    const urlRoot = "https://api.unsplash.com"
-    // const accessKey = process.env.REACT_APP_ACCESSKEY;
-    //${accessKey}
-
-    axios
-    .get(`${urlRoot}/photos/random?client_id=uCVztwhHCl6R2CaU9OxSedsnbFy0CUr-kc8OxofkWZw&count=20`)
-    .then(response => setImages([...images, ...response.data]))
-  }
-  
   return (
     <div className="App">
       
       <Heading />
-      <InfiniteScroll
-        dataLength={images.length}
-        next={fetchImages}
-        hasMore={true}
-        loader={null}
-      >
-
-        <div className='WrapperImage' >
-          {images.map(image => (
-            <UnsplashImage url={image.urls.thumb} key={image.id}/>
-          ))}
-        </div>
-
-
-      </InfiniteScroll>
+      <HomePage/>
     </div>
   )
 }
