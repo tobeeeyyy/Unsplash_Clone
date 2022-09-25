4ea79717bb47c0067d11466324f657b6a43c0257
Dev Pratap Singh <devsingh18003@gmail.com>
2022-09-25 06:30:58 +0530

    Added Image Search

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,45 @@
+
+import { createContext, useState } from "react"
 import Heading from "./components/Heading"
+import Images from "./components/Heading/Images"
+import SearchField from "./components/Heading/SearchField"
+import useAxios from "./components/hooks/useAxios"
 // import Loader from "./components/CSSLoader"
 
 import './global.css'
 import HomePage from './Page/HomePage'
 
+//Create Context
+export const ImageContext = createContext();
 
 function App() {
+
+  const [searchImage, setSearchImage] = useState('');
+  const {response, isLoading, error, fetchData} = useAxios(`search/photos?page=1&query=cats&client_id=uCVztwhHCl6R2CaU9OxSedsnbFy0CUr-kc8OxofkWZw`)
+  
+  const value = {
+    response,
+    isLoading,
+    error,
+    fetchData,
+    searchImage,
+    setSearchImage
+  }
 
   return (
     <div className="App">
       
-      <Heading />
+      <ImageContext.Provider value={value}>
+        <Heading>
+          <SearchField />
+        </Heading>
+        <Images />
+      </ImageContext.Provider>
+
       <HomePage/>
     </div>
   )
 }
+
 
 export default App
